0c0e8efaf845a676a4243384d19d835459cfa196
reingram <reingramsma@fastmail.fm>
2014-05-28 12:56:27 +0200

    third attempt to upload iDeal gateway

---
+++ library/Shineisp/Banks/Ideal/Gateway.php
@@ -0,0 +1,199 @@
+<?php
+
+/*
+ * Shineisp_Banks_Ideal_Gateway
+* -------------------------------------------------------------
+* Type:     class
+* Name:     Shineisp_Banks_Ideal_Gateway
+* Purpose:  Manage the communications with TargetPay for iDeal transactions
+* -------------------------------------------------------------
+*/
+
+class Shineisp_Banks_Ideal_Gateway extends Shineisp_Banks_Abstract implements Shineisp_Banks_Interface {
+
+		public function __construct($orderid) {
+		parent::__construct ( $orderid );
+		parent::setModule ( __CLASS__ );
+	}
+	
+	/**
+	 * CreateForm
+	 * Create the bank form
+	 * @return string
+	 */
+	public function CreateForm() {
+		$order = self::getOrder ();
+		$bank = self::getModule ();
+		$translator = self::getTranslator ();
+		
+		if (empty ( $bank ['account'] )) {
+			return null;
+		}
+		
+		if ($order) {
+		
+		$form = '';
+# Targetpay parameters	
+		require_once('func.php');
+		$item_name = $translator->translate ( "Order No." ) . " " . self::getOrderID() . " - " . date ( 'Y' );	
+		$rtlo = 110519;//accountnumber!!
+		$description = substr(trim($item_name),0,32);// max 32 char
+		$amount = $order ['grandtotal']*100;
+		$returnurl = 'http://'.self::getUrlOk();
+		//$returnurl = 'http://localhost.domeinnamme.com/cms/2.php';
+		// evt. aan $returnurl nog bijv. $amount en $description toevoegen voor evt. verdere verwerking!!  
+		//$reporturl = 'http://'.self::getUrlCallback();		
+# /Targetpay parameters		
+		try {
+			if (! self::isHidden ()) {
+				
+				$form .= '<table width="95%"><tr><td width="50px" valign="top"> ';
+				$form .= '<img src="/skins/default/base/images/banks/ideal.gif" border="0" alt="' . $translator->translate ( 'Pay Now' ) . ": " . $bank ['name'] . '" title="' . $translator->translate ( 'Pay Now' ) . ": " . $bank ['name'] . '" width="48px">';
+			}			
+			
+			$form .= '</td><td align="left">';	
+			$form .= '<p>Betaal met iDeal via uw eigen bank<br />';
+			$form .= '<form name="bankselect">';
+			$form .= '<span style="font-size:14px; font-weight: bold;">Kies eerst uw bank: </span>';
+			$form .= '<select name=bank onChange="document.bankselect.submit();">';
+			$form .= '<script src="https://www.targetpay.com/ideal/issuers-nl.js"></script>';
+			$form .= '</select>';
+$form .= '</form>';
+			$form .= '</p>NB: Onze iDeal betalingen worden verwerkt door <a href="https://www.targetpay.com/info/secure" target="_blank">Targetpay</a>. Dit wordt ook op uw bankafschrift vermeld.</td></tr></table>';
+			$form .= 'Onderstaande banken bieden de mogelijkheid tot iDeal betalingen';		
+			$form .= '<table width="100%"><tr>';
+			$form .= '<td><img src="/skins/default/base/images/banks/logo/banklogo_abnamro.png"></td>';
+			$form .= '<td><img src="/skins/default/base/images/banks/logo/banklogo_rabo.png"></td>';
+			$form .= '<td><img src="/skins/default/base/images/banks/logo/banklogo_friesland.png"></td></tr>';
+			$form .= '<tr><td><img src="/skins/default/base/images/banks/logo/banklogo_regio.png"></td>';
+			$form .= '<td><img src="/skins/default/base/images/banks/logo/banklogo_ing.png"></td> ';
+			$form .= '<td><img src="/skins/default/base/images/banks/logo/banklogo_sns.png"></td></tr>';
+			$form .= '<tr><td><img src="/skins/default/base/images/banks/logo/banklogo_asn.png"></td>';
+			$form .= '<td><img src="/skins/default/base/images/banks/logo/banklogo_knab.png"></td>';
+			$form .= '<td><img src="/skins/default/base/images/banks/logo/banklogo_lanschot.png"></td><tr>';
+			$form .= '<tr><td></td> ';
+			$form .= '<td><img src="/skins/default/base/images/banks/logo/banklogo_triodos.png"></td> ';
+			$form .= '<td></td></tr> ';
+			$form .= '</table>';
+			if (isset($_GET['bank'] ))
+			{
+				$urltp = StartTransaction($rtlo, $_GET['bank'], $description,$amount, $returnurl, $reporturl);
+
+				header( "Location: ". $urltp ) ;
+		 		exit();
+			}			
+			//return array('name' => $bank ['name'], 'description' => $bank ['description'], 'html' => $form);
+			return $form;
+		} catch ( Exception $e ) {
+			die ( $e->getMessage () );
+		}
+	}
+}
+
+	/**
+	 * Response
+	 * Create the Order, Invoice and send an email to the customer
+	 * @param $response from the Gateway Server
+	 * @return order_id or false
+	 */
+	public function Response($response) {
+		$bank = self::getModule ();
+		$bankid = $bank ['bank_id'];
+		require_once('func.php');
+		// De consument komt vanaf de bank terug op de returnurl.
+		// Hier controleren we de transactiestatus
+		//if( isset($_GET['ec']) && isset($_GET['trxid']))
+		if ((! empty ( $response ['trxid'])) && (! empty( $response['ec'])))
+		{
+			// 000000 OK betekent succesvol. We kunnen het product leveren
+			if(($status = CheckReturnurl( $rtlo, $response['trxid'] ))=="000000 OK" )
+			{
+				// Voeg hier programmacode toe om de orderstatus bij te werken.
+				
+				$payment = Payments::addpayment ( $orderid, $response ['trx_id'], $bankid, 0, $amount );
+				Orders::set_status ( $orderid, Statuses::id("paid", "orders") ); // Paid
+				OrdersItems::set_statuses ( $orderid, Statuses::id("paid", "orders") ); // Paid
+												
+				return $orderid;
+				//die( "Status was Successful...<br>Thank you for your order" );
+			}
+		// Bij alle andere statussen producten niet leveren
+		// Voeg hier zelf programmacode toe om de status bij te werken
+		else die( $status );
+		}
+		
+		// De reporturl wordt vanaf de Targetpay server aangeroepen
+		if ( isset($_POST['rtlo'])&&isset($_POST['trxid'])&& isset($_POST['status'])) 
+		{
+		HandleReporturl($_POST['rtlo'], $_POST['trxid'], $_POST['status'] );
+		}
+
+		/*
+		if (! empty ( $response ['item_number'] )) {
+			
+			// Get the indexes of the order 
+			$orderid = trim ( $response ['custom'] );
+			
+			if (is_numeric ( $orderid ) && is_numeric ( $bankid )) {
+				
+				// Replacing the comma with the dot in the amount value. 
+				$amount = str_replace ( ",", ".", $response ['amount'] );
+				
+				$payment = Payments::addpayment ( $orderid, $response ['thx_id'], $bankid, 0, $amount );
+				Orders::set_status ( $orderid, Statuses::id("paid", "orders") ); // Paid
+				OrdersItems::set_statuses ( $orderid, Statuses::id("paid", "orders") ); // Paid
+				
+
+				return $orderid;
+			}
+		}
+	*/
+	}
+
+	/**
+	 * CallBack
+	 * This function is called by the bank server in order to confirm the transaction previously executed
+	 * @param $response from the Gateway Server
+	 * @return boolean
+	 */
+	public function CallBack($response) {
+		Shineisp_Commons_Utilities::logs ( "Start callback", "ideal.log" );
+	// De consument komt vanaf de bank terug op de returnurl.
+		$bank = self::getModule ();
+		//$bankid = $bank ['bank_id'];
+		require_once('func.php');
+		$rtlo = 110519;
+		echo '<pre>';
+		print_r($response);
+		echo '</pre>';
+		//http://localhost.domeinnamme.com/common/callback/gateway/7b4c0cb89796ad9a06d2277bae78a4b9?trxid=0030000824438908&ec=215316177138874
+		// Hier controleren we de transactiestatus
+		
+		
+		//if( isset($_GET['ec']) && isset($_GET['trxid'])){
+		// 000000 OK betekent succesvol. We kunnen het product leveren
+		$inttrxid = $response['trxid'];
+		$intec = $response['ec'];
+		echo $inttrxid;
+		if(isset($intec) && isset($inttrxid ))
+		{
+		//if(($status = CheckReturnurl( $rtlo, $_GET['trxid'] ))=="000000 OK" ){
+		if(($status = CheckReturnurl( $rtlo, $inttrxid ))=="000000 OK" )
+		{		
+		echo '// Voeg hier programmacode toe om de orderstatus bij te werken.';
+		Shineisp_Commons_Utilities::logs ( "Callback parameters: ".$inttrxid.": ".$status, "ideal.log" );
+		
+		die( "Status was Successful...<br>Thank you for your order" );
+		}
+		
+		else 
+			{echo 'Product niet leveren';
+			Shineisp_Commons_Utilities::logs ( "Callback parameters: ".$inttrxid.": ".$status, "ideal.log" );
+			// Bij alle andere statussen producten niet leveren
+		// Voeg hier zelf programmacode toe om de status bij te werken
+				die( $status );
+			
+			}
+		}
+	}
+}
---
+++ library/Shineisp/Banks/Ideal/func.php
@@ -0,0 +1,105 @@
+<?php
+// deze functie werkniet goed met het php bestand, wel met het javascript (de bovenste)
+function SelectBank()
+        {
+        $urltp="https://www.targetpay.com/ideal/getissuers.php";
+        $strResponse = httpGetRequest($urltp);
+       
+        echo "<form name=\"bankselect\">Kies uw bank: ";
+        echo "<select name=bank onChange=\"document.bankselect.submit();\">";
+        echo "<script src=\"https://www.targetpay.com/ideal/issuers-nl.js\"></script>";
+        echo "</select></form>";
+        }
+/*
+function SelectBank()
+        {
+        $urltp="https://www.targetpay.com/ideal/getissuers.php";
+        $strResponse = httpGetRequest($urltp);
+        echo "<html>";
+        echo "Betaal veilig en gemakkelijk met iDeal.<br />";
+        echo "<form method=\"get\" name=\"idealform\">";
+        echo "<select name=\"bank\">".$strResponse."</select> ";
+        echo "<INPUT TYPE=\"submit\" VALUE=\"Verder gaan...\"></form>";
+        echo "NB: Uw iDeal betaling wordt verwerkt door <a href=\"https://www.targetpay.com/info/secure\" target=\"_blank\">Targetpay</a>. Dit wordt ook op uw bankafschrift vermeld.";
+        echo "</html>";
+        }
+*/
+function StartTransaction( $rtlo, $bank, $description, $amount, $returnurl, $reporturl)
+        {
+        $urltp= "https://www.targetpay.com/ideal/start?"."rtlo=".$rtlo."&bank=".$bank."&description=".urlencode($description)."&amount=".$amount."&returnurl=".urlencode($returnurl)."&reporturl=".urlencode($reporturl);
+        echo $urltp."<br />";
+        $strResponse = httpGetRequest($urltp);
+        $aResponse = explode('|', $strResponse );
+
+        //echo "<pre>";
+        //print_r($strResponse);
+        //echo "</pre>";
+        // error logger
+/*		$date = date("Y-m-d.H:i:s");
+		$uitvoer = "Response: ".$strResponse;
+			$logfile = PUBLIC_PATH.'error.log';
+			$filehandle = fopen($logfile, 'a') or die("can't open error file");
+			$logdata = $date.' - '.$uitvoer ."\n";
+                        fwrite($filehandle, $logdata);
+			fclose($filehandle);
+        echo $uitvoer;
+*/
+                # Bad response
+                if ( !isset ( $aResponse[1] ) ) die('Error' . $aResponse[0] );
+                $responsetype = explode ( ' ', $aResponse[0] );
+                $trxid = $responsetype[1];
+
+        // Hier kunt u het transactie id aan uw order toevoegen.
+        if( $responsetype[0] == "000000" ) return $aResponse[1];
+        else die($aResponse[0]);
+        }
+
+function CheckReturnurl($rtlo, $trxid)
+        {
+        $once=1;
+        $test=1; // Set to 1 for testing as described in paragraph 1.3; 0 for live site
+        $urltp= "https://www.targetpay.com/ideal/check?"."rtlo=".$rtlo."&trxid=".$trxid."&once=".$once."&test=".$test;
+        return httpGetRequest($urltp);
+        }
+
+function HandleReporturl($rtlo, $trxid, $status )
+        {
+                if( substr($_SERVER['REMOTE_ADDR'],0,10) == "89.184.168" )
+                {
+                // Update uw orderstatus hier
+                // ........
+                // De reporturl hoort OK terug te geven aan Targetpay.
+                die( "OK" );
+                }
+                else
+                {
+                die("IP address not correct... This call is not from Targetpay");
+                }
+        }
+
+function httpGetRequest($urltp)
+{
+        //$ch = curl_init($urltp);
+        //curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1) ;
+        //$strResponse = curl_exec($ch);
+        //curl_close($ch);
+        $strResponse = file_get_contents($urltp);
+        //echo "=> ".$ch." |  ".$strResponse."| ";
+                if ( $strResponse === false )
+                {die("Could not fetch response " . $urltp );}
+//                $strResponse = '<option selected value="">Kies uw bank...</option> <option value="0031">ABN Amro</option> <option value="0091">Friesland Bank</option> <option value="0721">ING</option> <option value="0021">Rabobank</option> <option value="0751">SNS Bank</option> <option value="0761">ASN Bank</option> <option value="0801">Knab</option> <option value="0771">RegioBank</option> <option value="0511">Triodos Bank</option> <option value="0161">Van Lanschot Bankiers</option>';
+
+        return $strResponse;
+        }
+
+function curPageURL()
+{
+$isHTTPS = (isset($_SERVER["HTTPS"]) && $_SERVER["HTTPS"] == "on");
+$port = (isset($_SERVER["SERVER_PORT"]) && ((!$isHTTPS && $_SERVER["SERVER_PORT"] != "80") || ($isHTTPS && $_SERVER["SERVER_PORT"] != "443")));
+$port = ($port) ? ':'.$_SERVER["SERVER_PORT"] : '';
+$pageURL = ($isHTTPS ? 'https://' : 'http://').$_SERVER["SERVER_NAME"].$port.$_SERVER["REQUEST_URI"];
+return $pageURL;
+}
+
+
+?>
